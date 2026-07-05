165c1275e169c9fb6031f9aebe7f6e81b057ad08
I-am-Chittesh <iamchitteshd@gmail.com>
2026-07-05 15:34:30 +0530

    launch height

--- final_assemble_rover_description/launch/gazebo.launch.py
+++ final_assemble_rover_description/launch/gazebo.launch.py
@@ -33,7 +33,7 @@ def generate_launch_description():
     spawn_entity = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        arguments=['-topic', 'robot_description', '-entity', 'my_rover'],
+        arguments=['-topic', 'robot_description', '-entity', 'my_rover', 'z', '0.5'],
         output='screen'
     )
     
