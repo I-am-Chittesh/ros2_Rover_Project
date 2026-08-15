f978d03f41bb6550825cbed0ea0daeeaf5ef4df8
I-am-Chittesh <iamchitteshd@gmail.com>
2026-08-15 13:39:23 +0530

    gazebo_launch

--- rover_assembly_description/launch/gazebo.launch.py
+++ rover_assembly_description/launch/gazebo.launch.py
@@ -55,7 +55,13 @@ def generate_launch_description():
     spawn_entity_node = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        arguments=['-topic', 'robot_description', '-entity', 'rover_assembly'],
+        arguments=['-topic', 'robot_description', '-entity', 'rover_assembly',
+        'x', '-3.400000', 
+        'y', '-3.400000',
+        'z', '0.033876',
+        'R', '-0.001036',
+        'P', '-0.050862',
+        'Y', '2.082935'],
         output='screen'
     )
 
